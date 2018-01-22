dbcd466fa884497904012af818f493f92c2f9075
Frac7 <francescacella96@gmail.com>
2018-01-23 00:22:17 +0100

    Quarta activity

--- app/src/main/java/ium/project/clanmanagerforclashroyale/ClanManager.java
+++ app/src/main/java/ium/project/clanmanagerforclashroyale/ClanManager.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Home extends AppCompatActivity
+public class ClanManager extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_clan_manager);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
